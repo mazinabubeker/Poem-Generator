8daaf17e91a21cf5e0e7d5db4e5ab7ab7193bde1
Mazin Abubeker <mabubeke@ucsd.edu>
2019-04-27 17:54:15 -0700

    v1.0

---
+++ create_app.js
@@ -0,0 +1,211 @@
+// Data
+//window.onbeforeunload = function () {return false;}
+var nouns = [];
+var adjectives = [];
+var verbs = [];
+var adverbs = [];
+var prepositions = [];
+var fullPoem = "";
+var stage = 0;
+var canGoNext = true;
+var database = firebase.database();
+
+var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+function start(){
+    $("#poem").text("asdlkasdkjaslkdjlakdsklad");
+}
+
+start();
+
+function writeUserData(title, poemText, date) {
+  database.ref('poems/' + title).set({
+    poem: poemText,
+    date: date,
+  });
+}
+
+var regex = /^[A-Za-z]+$/;
+
+function nextClick(){
+    var valid = [];
+    valid[0] = regex.test($("#data1").val());
+    valid[1] = regex.test($("#data2").val());
+    valid[2] = regex.test($("#data3").val());
+    if(valid[0] && valid[1] && valid[2] && canGoNext){
+//        $("input").css({
+//            'border-bottom-color': 'rgba(0, 0, 0, .4)'
+//        });
+        $("input").removeAttr('style');
+        
+        $("#errorInstruction").animate({
+            opacity: 0
+        }, 250);
+        
+        if(stage == 0){
+            nouns.push($("#data1").val());
+            nouns.push($("#data2").val());
+            nouns.push($("#data3").val());
+            triggerNextInput("adjectives");
+        }else if(stage == 1){
+            adjectives.push($("#data1").val());
+            adjectives.push($("#data2").val());
+            adjectives.push($("#data3").val());
+            triggerNextInput("verbs");
+        }else if(stage == 2){
+            verbs.push($("#data1").val());
+            verbs.push($("#data2").val());
+            verbs.push($("#data3").val());
+            triggerNextInput("adverbs");
+        }else if(stage == 3){
+            adverbs.push($("#data1").val());
+            adverbs.push($("#data2").val());
+            adverbs.push($("#data3").val());
+            triggerNextInput("prepositions");
+        }else if(stage == 4){
+            prepositions.push($("#data1").val());
+            prepositions.push($("#data2").val());
+            prepositions.push($("#data3").val());
+            finishInput();
+        }
+        stage++;
+    }else{
+        if($("#errorInstruction").css("opacity") == 1){
+            $("#errorInstruction").animate({
+                'font-size': '22px'
+            }, 50);
+            $("#errorInstruction").animate({
+                'font-size': '20px'
+            }, 50);
+            
+        }else{
+            if(canGoNext){
+                $("#errorInstruction").animate({
+                    'opacity': '1'
+                }, 250);
+            }
+        }
+        
+        if(!valid[0]){
+            $("#data1").css({
+                'border-bottom-color': 'rgba(255, 0, 0, .6)'
+            });
+        }else{
+//            $("#data1").css({
+//                'border-bottom-color': 'rgba(0, 0, 0, .4)'
+//            });
+            $("#data1").removeAttr('style');
+        }
+        
+        if(!valid[1]){
+            $("#data2").css({
+                'border-bottom-color': 'rgba(255, 0, 0, .6)'
+            });
+        }else{
+//            $("#data2").css({
+//                'border-bottom-color': 'rgba(0, 0, 0, .4)'
+//            });
+            $("#data2").removeAttr('style');
+        }
+        
+        if(!valid[2]){
+            $("#data3").css({
+                'border-bottom-color': 'rgba(255, 0, 0, .6)'
+            });
+        }else{
+//            $("#data3").css({
+//                'border-bottom-color': 'rgba(0, 0, 0, .4)'
+//            });
+            $("#data3").removeAttr('style');
+        } 
+    }
+}
+
+function triggerNextInput(type){
+    canGoNext = false;
+    $("input").animate({
+        'opacity': '0' 
+    }, 500, function(){
+        $("#data1").val("")
+        $("#data2").val("")
+        $("#data3").val("")
+    });
+    
+    $("input").animate({
+        'opacity': '1'
+    }, 500);
+    
+    $("#instruction").animate({
+        'opacity': '0'
+    }, 500, function(){
+        $("#instruction").text("Choose 3 " + type);
+    });
+    
+    $("#instruction").animate({
+        opacity: 1
+    }, 500, function(){
+        canGoNext = true;
+    });
+    
+    
+}
+
+function finishInput(){
+    fullPoem = "";
+    for(var i = 0; i < 100; i++){
+        fullPoem = fullPoem + nouns[i%3] + " ";
+        fullPoem = fullPoem + adjectives[i%3] + " ";
+        fullPoem = fullPoem + verbs[i%3] + " ";
+        fullPoem = fullPoem + adverbs[i%3] + " ";
+        fullPoem = fullPoem + prepositions[i%3] + " ";
+        fullPoem = fullPoem + " and then ";
+    }
+    
+    $("#poemCreator").animate({
+        'opacity': '0'
+    }, 500, function(){
+        $("#poemCreator").css({
+            'display': 'none'
+        });
+        
+        $("#poem").text(fullPoem);
+    
+        $("#finalizePoem").css({
+            'display': 'inline'
+        });
+
+        $("#finalizePoem").animate({
+            'opacity': '1'
+        }, 500);
+        
+    });
+    
+    
+}
+
+function submitPoem(){
+    var curDate = new Date();
+    var month = months[curDate.getMonth()];
+    var date = curDate.getDate();
+    var year = curDate.getFullYear();
+    writeUserData($("#titleInput").val(), fullPoem, month + " " + date.toString(10) + ", " + year.toString(10));
+    
+    $("#finalizePoem").animate({
+        'opacity': '0'
+    }, 100, function(){
+        window.location.href="poems.html";
+    });
+}
---
+++ poems_app.js
@@ -0,0 +1,84 @@
+var database = firebase.database();
+
+var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+var cardComponent = `<div class="cardTrigger">
+                        <div class="card">
+                            <div class="cardTitle">
+                            </div>
+                            <div class="cardDate">
+                            </div>
+                            <div class="cardContent">
+                            </div>
+                        </div>
+                     </div>`;
+
+//var cardComponent = `<div class="cardTrigger">
+//                        <div class="card">
+//                        </div>
+//                     </div>`;
+
+$(document).ready(function() {
+    pullCards();
+});
+
+function pullCards(){
+    firebase.database().ref('/poems/').once('value').then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot){
+            var cur = $(cardComponent).appendTo("#cardView");
+            cur.children(".card").children(".cardTitle").text(childSnapshot.key);
+            cur.children(".card").children(".cardDate").text(childSnapshot.child("date").val());
+            cur.children(".card").children(".cardContent").text(childSnapshot.child("poem").val());
+        });
+    });
+}
+
+function newPoem(){
+    window.location.href="create.html";
+}
+
+function deletePoems(){
+    $(".card").animate({
+        'opacity': '0',
+        'transform': 'scale(.8)'
+    }, 200, function(){
+        $(".cardTrigger").remove();
+        firebase.database().ref('poems/').set({});
+    });
+}
+
+function testPoem(){
+    var curDate = new Date();
+    var month = months[curDate.getMonth()];
+    var date = curDate.getDate();
+    var year = curDate.getFullYear();
+    
+    var testTitle = "Test poem " + (999 + Math.floor(Math.random()*9000)).toString(10);
+    var testDate = month + " " + date.toString(10) + ", " + year.toString(10);
+    var testPoem = `
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                `;
+    database.ref('poems/' + testTitle).set({
+        poem: testPoem,
+        date: testDate,
+    });
+    var cur = $(cardComponent).appendTo("#cardView");
+    cur.children(".card").children(".cardTitle").text(testTitle);
+    cur.children(".card").children(".cardDate").text(testDate);
+    cur.children(".card").children(".cardContent").text(testPoem);
+    
+    
+}
