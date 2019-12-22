e770c934532b581cd9a59099bde1b357d020650e
Mazin Abubeker <mazin.abubeker@gsvlabs.com>
2019-12-22 15:12:54 -0800

    pad test

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let colors = ["red", "blue", "pink", "green", "yellow"];
 function touched(){
-    document.getElementById('pad').style.backgroundColor = colors[Math.floor(random()*colors.length)];
+    if(document.getElementById('pad').style.backgroundColor == "blue"){
+        document.getElementById('pad').style.backgroundColor = "red";
+    }else{
+        document.getElementById('pad').style.backgroundColor = "blue";
+    }
 }
