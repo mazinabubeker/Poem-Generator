2ff7a7c7618fb1175d6ea47f67974734ab26a2d6
Mazin Abubeker <mazin.abubeker@gsvlabs.com>
2019-12-23 01:10:27 -0800

    yes

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 var instruments = [];
 
 window.onload = e => {
-    instruments.push(loadSound('kick.wav'));
-    instruments.push(loadSound('snare.wav'));
-    instruments.push(loadSound('hat.wav'));
+//    instruments.push();
+//    instruments.push(loadSound('snare.wav'));
+//    instruments.push(loadSound('hat.wav'));
 }
 
 function preload(){
 //    document.body.bind('touchmove', function(event) { event.preventDefault()});
-    
+    let a = loadSound('kick.wav');
+    instruments.push(a);
 }
 
 function pressed(i){
