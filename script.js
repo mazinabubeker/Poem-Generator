2fbaf3dfbe290e26f7fad14634e266e669273541
Mazin Abubeker <mazin.abubeker@gsvlabs.com>
2019-12-23 13:40:43 -0800

    fixv2

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ var ready;
 
 function preload(){
     ready = false;
+    instruments.push(loadSound('sounds/low_tom.wav'));
+    instruments.push(loadSound('sounds/mid_tom.wav'));
+    instruments.push(loadSound('sounds/high_tom.wav'));
+    instruments.push(loadSound('sounds/crash.wav'));
     instruments.push(loadSound('sounds/kick.wav'));
     instruments.push(loadSound('sounds/snare.wav'));
     instruments.push(loadSound('sounds/hat.wav'));
+    instruments.push(loadSound('sounds/open_hat.wav'));
 }
 
 function setup(){
