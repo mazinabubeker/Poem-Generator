8cbc07077b1962a440f73a78a50d8a7ef28a7b1e
Mazin Abubeker <mazin.abubeker@gsvlabs.com>
2019-12-23 14:12:52 -0800

    fixv2

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ function setup(){
         e.preventDefault();
     });
     
-    window.addEventListener('scroll', e=>{
-        window.scrollTo(0, 0);
-    });
+//    window.addEventListener('scroll', e=>{
+//        window.scrollTo(0, 0);
+//    });
     
-    window.removeEventListener('scroll', e=>{
-        window.scrollTo(0, 0);
-    });
+//    window.removeEventListener('scroll', e=>{
+//        window.scrollTo(0, 0);
+//    });
     
     $('pad').addEventListener('touchstart', e=>{
         this.style.backgroundColor = "#89c79b";
